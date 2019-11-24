96df8aa23b378a5edd66aa326e028348ab2d8ad2
Beking <sbqoo@qq.com>
2019-11-24 12:43:39 +0800

    第十九篇: 能不能模拟实现一个 bind 的效果？

--- 原生JS灵魂之问/bind.js
+++ 原生JS灵魂之问/bind.js
@@ -52,3 +52,14 @@ Function.prototype.bind = function(context, ...args) {
 
   return fbound;
 };
+
+Function.prototype.myBind = function(context, ...args) {
+  const fn = this;
+  args = args ? args : [];
+  return function newFn(...newFnArgs) {
+    if (this instanceof newFn) {
+      return new fn(...args, ...newFnArgs);
+    }
+    return fn.apply(context, [...args, ...newFnArgs]);
+  };
+};
