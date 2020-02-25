53660009536ca91efd52a5730293d4bbd8f9e4cb
Beking <sbqoo@qq.com>
2020-02-26 00:14:32 +0800

    flask web

---
+++ flask_demo/index.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    sentence = request.form.get("sentence")
+    if sentence == '':
+        return render_template('index.html', tips='tips: please input the sentence')
+
+    result = {
+        "sentence": sentence,
+        "len": 5,
+        # phrase label start end nested_level
+        "phrase": [(["beking"], "PER", 3, 4, 1),
+                   (["is", "beking"], "PER", 2, 4, 2),
+                   (["name"], "ORG", 1, 2, 1),
+                   (["my", "name"], "ORG", 0, 2, 2),
+                   (["my", "name", "is", "beking"], "FAC", 0, 4, 3)]
+    }
+    
+    return render_template('index.html', result=result, sentence=sentence)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
