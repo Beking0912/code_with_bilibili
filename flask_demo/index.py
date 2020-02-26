44922ca3780c618319ad701f58e09ea3245145dd
Beking <sbqoo@qq.com>
2020-02-27 01:58:57 +0800

    最终完美版本

--- flask_demo/index.py
+++ flask_demo/index.py
@@ -22,12 +22,14 @@ def search():
         return render_template('index.html', tips='tips: please input the sentence', result=result)
 
     result = {
-        "sentence": ["my", "name", "is", "beking"],
+        "sentence": ["my", "name", "is", "beking","my", "name", "is", "beking","my", "name", "is", "beking","my", "name", "is", "beking","my", "name", "is", "beking"],
         # phrase label start end nested_level
         "phrase": [(["beking"], "PER", 3, 4, 1),
                    (["is", "beking"], "PER", 2, 4, 2),
                    (["name"], "ORG", 1, 2, 1),
-                   (["my", "name", "is", "beking"], "FAC", 0, 3, 3)]
+                   (["my", "name", "is", "beking"], "FAC", 0, 3, 3),
+                   (["my", "name", "is", "beking"], "LOC", 8, 10, 1),
+                   (["my", "name", "is", "beking"], "VEH", 14, 16, 1)]
     }
 
     return render_template('index.html', result=result, sentence=sentence)
