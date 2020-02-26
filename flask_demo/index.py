a2f68624fa68890c057e5926faa6a34a9a81f320
Beking <sbqoo@qq.com>
2020-02-26 23:45:46 +0800

    第二版 修复跳转bug 补充样式

--- flask_demo/index.py
+++ flask_demo/index.py
@@ -5,27 +5,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    result = {
+        "sentence": [],
+        "phrase": []
+    }
+    return render_template('index.html', result=result)
 
 @app.route('/search', methods=['POST'])
 def search():
     sentence = request.form.get("sentence")
     if sentence == '':
-        return render_template('index.html', tips='tips: please input the sentence')
+        result = {
+            "sentence": [],
+            "phrase": []
+        }
+        return render_template('index.html', tips='tips: please input the sentence', result=result)
 
     result = {
-        "sentence": sentence,
-        "len": 5,
+        "sentence": ["my", "name", "is", "beking"],
         # phrase label start end nested_level
         "phrase": [(["beking"], "PER", 3, 4, 1),
-                #    (["is", "beking"], "PER", 2, 4, 2),
+                   (["is", "beking"], "PER", 2, 4, 2),
                    (["name"], "ORG", 1, 2, 1),
-                #    (["my", "name"], "ORG", 0, 2, 2),
-                   (["my", "name", "is"], "FAC", 0, 3, 3)]
+                   (["my", "name", "is", "beking"], "FAC", 0, 3, 3)]
     }
 
-    
-    
     return render_template('index.html', result=result, sentence=sentence)
 
 
